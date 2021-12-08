d7cdb2c3af9ebdb3ccfef166d3073ca5a4507922
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2021-12-08 18:13:12 +0200

    1

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const fotmInput = document.querySelector('#validation-input');
 
 fotmInput.addEventListener("input", (event) => {
     fotmInput.addEventListener("blur", (event) => {
-        event.currentTarget.value.length === Number(fotmInput.dataset["length"]) ?
+        event.currentTarget.value.length === Number(fotmInput.dataset.length) ?
             fotmInput.id = 'validation-input__valid':
             fotmInput.id = 'validation-input__invalid';
      });
